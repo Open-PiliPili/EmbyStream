6ef8e86cdaa2ffb29bd04ee5bae2d2453dcd1d11
Open-PiliPili <openpilipili@gmail.com>
2025-07-25 12:08:08 +0800

    fix(build): remove mistakenly added config.toml and fix FFmpeg path on Linux

--- src/build.rs
+++ src/build.rs
@@ -4,8 +4,8 @@ fn main() {
     } else if cfg!(all(target_os = "macos", target_arch = "x86_64")) {
         println!("cargo:rustc-env=FFMPEG_DIR=/usr/local");
     } else if cfg!(all(target_os = "linux", target_arch = "aarch64")) {
-        println!("cargo:rustc-env=FFMPEG_DIR=/opt/ffmpeg_arm");
+        println!("cargo:rustc-env=FFMPEG_DIR=/usr/bin");
     } else if cfg!(all(target_os = "linux", target_arch = "x86_64")) {
-        println!("cargo:rustc-env=FFMPEG_DIR=/opt/ffmpeg_x86");
+        println!("cargo:rustc-env=FFMPEG_DIR=/usr/bin");
     }
 }
